6c7a01b6e7d19959d16af99ed2b674de2cdc714b
Roberto Calabrese <bingofree@MacBook-Pro-di-Roberto.local>
2018-02-18 21:48:03 +0100

    fix start

--- start.sh
+++ start.sh
@@ -83,11 +83,11 @@ next3(){
     cd ../
     git clone https://github.com/stefanprodan/swarmprom.git
     cd swarmprom
-    ADMIN_USER=admin \
-    ADMIN_PASSWORD=admin \
-    SLACK_URL=https://hooks.slack.com/services/TOKEN \
-    SLACK_CHANNEL=devops-alerts \
-    SLACK_USER=alertmanager \
+    export ADMIN_USER=admin \
+    export ADMIN_PASSWORD=admin \
+    export SLACK_URL=https://hooks.slack.com/services/TOKEN \
+    export SLACK_CHANNEL=devops-alerts \
+    export SLACK_USER=alertmanager \
     docker stack deploy -c docker-compose.yml mon
     next4
 }
